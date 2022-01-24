18bebffb9c8af9891bf83cb2ba1cd194c824f69d
owenlessel <owenlessel@gmail.com>
2022-01-24 09:59:30 -0600

    more updates

--- src/Heart.java
+++ src/Heart.java
@@ -6,12 +6,12 @@ public class Heart {
     private boolean available;
     private int stock;
     private int size;
-    private Heart[] sayings;
+    private String[] sayings;
 
     // CONSTRUCTORS
     public Heart() {} // This is the default contructor
     //Add overloaded Constructor here...
-    public Heart(String color,int calories,double costPerPound,boolean available,int stock,int size,Heart[] sayings){
+    public Heart(String color,int calories,double costPerPound,boolean available,int stock,int size,String[] sayings){
         this.color=color;
         this.costPerPound=costPerPound;
         this.stock=stock;
@@ -27,20 +27,39 @@ public class Heart {
     public int getSize(){
         return this.size;
     }
-    public Heart[] getsayings(){
+    public String[] getSayings(){
         return this.sayings;
     }
+    public int getCalories(){
+        return this.calories;
+    }
+    public double getCost(){
+        return this.costPerPound;
+    }
+    public boolean getAvailable(){
+        return this.available;
+    }
+    public int getStock(){
+        return this.stock;
+    }
+    
     
     // MUTATORS
     public void setColor(String color) {
         this.color = color;
     }
     //Add remaining Mutator methods here...
-
+    public void adjPrice(double costPerPound){
+        int increase=0;
+        this.costPerPound=costPerPound*increase;
+    }
+    public void inStock(int stock){
+        
+    }
 
     // METHODS
     public String report() {
-        return this.color + "\n"; // Add other properties here as you add them to the class
+        return this.color + "\n" + this.size + "\n" + this.sayings + "\n" + this.calories + "\n" + this.costPerPound + "\n" + this.available + "\n" + this.stock; // Add other properties here as you add them to the class
     }
     //Write other methods here (if needed)...
 }
--- src/HeartsClient.java
+++ src/HeartsClient.java
@@ -1,5 +1,7 @@
 public class HeartsClient {
     public static void main(String[] args) throws Exception {
-        
+        String[] sayings= {"Hi","Bye"};
+        Heart test1 = new Heart("red", 0, 1.50, true, 20, 5,sayings);
+        System.out.println(test1.report());
     }
 }
