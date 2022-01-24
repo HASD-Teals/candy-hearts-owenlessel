a2fd0f9ab536a4233518b4ceda4c695710d3b10a
owenlessel <owenlessel@gmail.com>
2022-01-24 09:10:48 -0600

    added properties

--- src/Heart.java
+++ src/Heart.java
@@ -1,21 +1,36 @@
 public class Heart {
     // PROPERTIES
     private String color;
-    //Add remaining heart properties here...
-
+    private int calories;
+    private double costPerPound;
+    private boolean available;
+    private int stock;
+    private int size;
+    private Heart[] sayings;
 
     // CONSTRUCTORS
     public Heart() {} // This is the default contructor
     //Add overloaded Constructor here...
-
+    public Heart(String color,int calories,double costPerPound,boolean available,int stock,int size,Heart[] sayings){
+        this.color=color;
+        this.costPerPound=costPerPound;
+        this.stock=stock;
+        this.size=size;
+        this.sayings=sayings;
+    }
 
     // ACCESSORS
     public String getColor() {
         return this.color;
     }
     //Add remaining Accessor methods here...
-
-
+    public int getSize(){
+        return this.size;
+    }
+    public Heart[] getsayings(){
+        return this.sayings;
+    }
+    
     // MUTATORS
     public void setColor(String color) {
         this.color = color;
